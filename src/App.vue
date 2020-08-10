7f4ee637615cdfce8ad29d07983b463036e28412
Vsevolod Zolotov <vsevolod.zolotov02@mail.ru>
2020-08-10 21:37:12 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  })
+}
+</script>
