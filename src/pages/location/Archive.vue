68be5baa78be865490f153cdef12d7abd6ebd470
vsezol <vsevolod.zolotov02@mail.ru>
2020-08-11 16:11:00 +0700

    sort by completed

--- src/pages/location/Archive.vue
+++ src/pages/location/Archive.vue
@@ -1,6 +1,10 @@
 <template>
   <v-card>
-    <archive-item v-for="(item, index) in archive" :key="index" v-bind="item">
+    <archive-item
+      v-for="(item, index) in completedToDown(archive)"
+      :key="index"
+      v-bind="item"
+    >
     </archive-item>
   </v-card>
 </template>
@@ -34,6 +38,20 @@ export default {
           result: ['60° 30 N', '49° 20 E']
         }
       ]
+    }
+  },
+  methods: {
+    completedToDown(arr) {
+      const newArr = []
+      const arrLen = arr.length
+      for (let i = 0; i < arrLen; i++) {
+        if (arr[i].completed) {
+          newArr.push(arr[i])
+        } else {
+          newArr.unshift(arr[i])
+        }
+      }
+      return newArr
     }
   },
   components: {
