7c510193fd93fd7f5dbc6becfc882eb8eafe7714
vsezol <vsevolod.zolotov02@mail.ru>
2020-08-17 19:14:24 +0700

    lang and brightness

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,14 +1,33 @@
 <template>
-  <v-card> </v-card>
+  <v-card class="pa-4">
+    <v-card-title class="px-0 pt-0">
+      <span class="ml-2">
+        <v-icon color="success">
+          mdi-cog
+        </v-icon>
+      </span>
+      <span class="ml-2">Settings</span>
+    </v-card-title>
+    <v-form class="settings-form mx-auto">
+      <v-select :items="items" label="Язык" outlined></v-select>
+      <v-slider v-model="brightness" prepend-icon="mdi-white-balance-sunny" />
+    </v-form>
+  </v-card>
 </template>
 
 <script>
-import PowerOffModal from '@/components/PowerOffModal'
-
 export default {
   name: 'Settings',
   data() {
-    return {}
+    return {
+      items: ['Русский', 'English'],
+      brightness: 50
+    }
   }
 }
 </script>
+
+<style lang="sass" scoped>
+.settings-form
+  max-width: 600px
+</style>
--- src/plugins/router.js
+++ src/plugins/router.js
@@ -3,12 +3,14 @@ import Router from 'vue-router'
 import Menu from '../pages/Menu'
 import location from './routes/location'
 import MeasureAngle from '../pages/MeasureAngle'
+import Settings from '../pages/Settings'
 
 Vue.use(Router)
 export default new Router({
   routes: [
     ...location,
     { path: '/measure-angle', component: MeasureAngle },
+    { path: '/settings', component: Settings },
     { path: '/', component: Menu }
   ],
   mode: 'history'
