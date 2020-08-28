e4b417d9d41a4036f78edd2471e2caebe4bbd4d7
Vsevolod Zolotov <vsevolod.zolotov02@mail.ru>
2020-08-28 23:49:36 +0700

    format T and OC

--- src/components/MeasurementItem.vue
+++ src/components/MeasurementItem.vue
@@ -5,10 +5,10 @@
     </v-list-item-icon>
     <v-list-item-content class="d-flex flex-wrap flex-row">
       <div class="parametr mr-4">
-        <span class="font-weight-bold">OC:</span> {{ OC }}
+        <span class="font-weight-bold">OC:</span> {{ OC | formatOC }}
       </div>
       <div class="parametr">
-        <span class="font-weight-bold">T:</span> {{ T }}
+        <span class="font-weight-bold">T:</span> {{ T | formatT}}
       </div>
       <div class="remove-it ml-auto" @click="$emit('remove', number)">
         <span><v-icon color="error">mdi-delete</v-icon></span>
@@ -24,6 +24,14 @@ export default {
     number: Number,
     OC: Number,
     T: Number
+  },
+  filters: {
+    formatOC: OC => parseInt(OC / 60) + '°' + (OC % 60) + "'",
+    formatT: T => {
+      const date = new Date(0)
+      date.setSeconds(T) 
+      return date.toISOString().substr(11, 8)
+    }
   }
 }
 </script>
